ccaf5832d458c62dcbec7b0b0e4f1f4760621b16
Ben Wilkes <ben@eclipseitconsulting.co.uk>
2024-10-24 11:26:24 +0100

    Adds custom metrics - fix 4

--- metrics.py
+++ metrics.py
@@ -1,6 +1,7 @@
 from aws_embedded_metrics import metric_scope
 from aws_embedded_metrics.storage_resolution import StorageResolution
 from os import environ
+from asyncio import get_event_loop
 from logging import getLogger
 
 logger = getLogger(__name__)
@@ -10,9 +11,16 @@ def metrics_enabled():
     return value != "local"
 
 @metric_scope
-def put_metric(metric_name, value, unit):
-    logger.info(f"Counter metric: {metric_name} - {value}")
-    metrics.put_metric(metric_name, value, unit, StorageResolution.STANDARD)
+def put_metric(metric_name, value, unit, metrics):
+    try:
+        logger.info(f"put metric: {metric_name} - {value} - {unit}")
+        metrics.put_metric(metric_name, value, unit, StorageResolution.STANDARD)
+    finally:
+        # Remove noise in logs
+        # https://github.com/awslabs/aws-embedded-metrics-python/issues/52
+        loop = get_event_loop()
+        loop.run_until_complete(metrics.flush())
+
 
 def counter(metric_name, value):
     if metrics_enabled():
